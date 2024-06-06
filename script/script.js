37b75dcddecb667dde7a30b0b562f0bb88f611a8
lemos000 <lemosgabriel2016@gmail.com>
2024-06-05 23:11:58 -0300

    Criação do form com validação usando regex

--- script/script.js
+++ script/script.js
@@ -5,6 +5,7 @@ window.addEventListener('load', function() {
     const typingSpeed = 150; 
     const erasingSpeed = 100; 
     const secTextoElement = document.querySelector('.sec-texto');
+    const cpfInput = document.getElementById('cpf');
 
     let index = 0;
     let isErasing = false;
@@ -43,3 +44,35 @@ window.addEventListener('load', function() {
     typeWriter();
 });
 
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const cpfInput = document.getElementById('cpf');
+    const telefoneInput = document.getElementById('telefone');
+
+    cpfInput.addEventListener('input', function (e) {
+        let value = cpfInput.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+        if (value.length > 11) {
+            value = value.slice(0, 11); // Limita o valor a 11 dígitos
+        }
+
+        // Aplica a máscara de CPF
+        value = value.replace(/(\d{3})(\d)/, '$1.$2');
+        value = value.replace(/(\d{3})(\d)/, '$1.$2');
+        value = value.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+
+        cpfInput.value = value;
+    });
+
+    telefoneInput.addEventListener('input', function (e) {
+        let value = telefoneInput.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+        if (value.length > 11) {
+            value = value.slice(0, 11); // Limita o valor a 11 dígitos
+        }
+
+        // Aplica a máscara de telefone
+        value = value.replace(/^(\d{2})(\d)/, '($1) $2');
+        value = value.replace(/(\d{5})(\d)/, '$1-$2');
+
+        telefoneInput.value = value;
+    });
+});
